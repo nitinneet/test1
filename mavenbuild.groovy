b62cbd1cd0f0e8dc35dd6880d0bdd6782d2bba02
marthanda93 <marthanda93@users.noreply.github.com>
2018-01-05 18:44:56 +0530

    Update mavenbuild.groovy

--- mavenbuild.groovy
+++ mavenbuild.groovy
@@ -10,25 +10,15 @@ def call(body) {
         deleteDir()
 
         try {
-            stage ('Clone') {
-                checkout scm
+            stage ('setup') {
+                pom = load "lib/pom.groovy"
             }
             stage ('Build') {
-                sh "echo 'building ${config.projectName} ...'"
-            }
-            stage ('Tests') {
-                parallel 'static': {
-                    sh "echo 'shell scripts to run static tests...'"
-                },
-                'unit': {
-                    sh "echo 'shell scripts to run unit tests...'"
-                },
-                'integration': {
-                    sh "echo 'shell scripts to run integration tests...'"
-                }
-            }
-            stage ('Deploy') {
-                sh "echo 'deploying to server ${config.serverDomain}...'"
+                git(url: ${config.giturl})
+
+                stage "Build"
+                tools.configureMaven()
+                sh("./mvnw spring-boot:run")
             }
         } catch (err) {
             currentBuild.result = 'FAILED'
@@ -36,3 +26,8 @@ def call(body) {
         }
     }
 }
+
+
+
+
+
