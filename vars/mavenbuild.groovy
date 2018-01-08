f1eaa6af3b62ea1d9ad613f85007318e25ec409f
marthanda93 <marthanda93@users.noreply.github.com>
2018-01-08 11:54:04 +0530

    Update mavenbuild.groovy

--- vars/mavenbuild.groovy
+++ vars/mavenbuild.groovy
@@ -11,7 +11,7 @@ def call(body) {
 
         try {
             stage ('Setup') {
-                git(url: "${config.giturl}")
+                git(url: "${config.giturl}", branch: 'master')
             }
             stage ('Build') {  
                 sh("${config.buildstep}")
@@ -33,3 +33,5 @@ def call(body) {
             currentBuild.result = 'FAILED'
             throw err
         }
+    }
+}
