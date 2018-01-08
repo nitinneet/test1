03d8f2a634befe04699e23ec768b7786b8565383
root <root@templateservice.localdomain>
2018-01-08 23:01:04 +0530

    test5 correct6

--- vars/anand.groovy
+++ vars/anand.groovy
@@ -7,6 +7,9 @@ def call(body) {
     body.delegate = config
     body()
 
+    Yaml parser = new Yaml()
+    Map pipelineDefinition = parser.load(new File(pwd() + 'pipeline.yml').text)
+
     node {
       adm = new scm.Git(this,config)
       adm.checkout()
