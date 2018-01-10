07c876cc08443ee8c77804a20c6b55826088ba23
root <root@templateservice.localdomain>
2018-01-11 04:23:35 +0530

    safir jenkins param

--- src/org/k9/safir/Params.groovy
+++ src/org/k9/safir/Params.groovy
@@ -7,12 +7,13 @@ class Params {
   Params(script,config) {
     this.config = config
     this.script = script
+    this.params = "anand kumar"
 
+/*
     this.params = this.script.parameters {
         string(defaultValue: "TEST", description: 'What environment?', name: 'userFlag')
         choice(choices: 'US-EAST-1\nUS-WEST-2', description: 'What AWS region?', name: 'region')
     }
-/*
     this.params = this.script.input ( id: 'params', parameters: [
       [$class: 'ChoiceParameterDefinition',name: 'CLUSTER_APP_DOMAIN',choices: 'https://master.marthanda.in',description: ''],
       [$class: 'CredentialsParameterDefinition', credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl', defaultValue: '', description: '',name: 'openShiftToken',required: true]
