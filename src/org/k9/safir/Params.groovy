cc49ca069654d4bd1fe74209b5543fd5c91fe4bb
root <root@templateservice.localdomain>
2018-01-11 04:18:37 +0530

    safir jenkins param

--- src/org/k9/safir/Params.groovy
+++ src/org/k9/safir/Params.groovy
@@ -8,7 +8,7 @@ class Params {
     this.config = config
     this.script = script
 
-    this.params = parameters {
+    this.params = this.script.parameters {
         string(defaultValue: "TEST", description: 'What environment?', name: 'userFlag')
         choice(choices: 'US-EAST-1\nUS-WEST-2', description: 'What AWS region?', name: 'region')
     }
