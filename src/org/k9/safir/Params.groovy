6ac5741ee909b04ee35aa878499e2ae178e7b53d
root <root@templateservice.localdomain>
2018-01-11 04:27:30 +0530

    safir jenkins param

--- src/org/k9/safir/Params.groovy
+++ src/org/k9/safir/Params.groovy
@@ -3,15 +3,14 @@ package org.k9.safir
 class Params {
   def config
   def script
-  def params = "anand"
 
   Params(script,config) {
     this.config = config
     this.script = script
-    this.params = params
   }
 
   void build() {
-      return this.params
+      def params = "anand"
+      return params
   }
 }
