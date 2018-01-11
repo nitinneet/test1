5dcaa18c28df54316c2103f7c970fd22b5414a3b
root <root@templateservice.localdomain>
2018-01-11 05:43:19 +0530

    safir jenkins param

--- src/org/k9/safir/Params.groovy
+++ src/org/k9/safir/Params.groovy
@@ -8,13 +8,12 @@ class Params {
   Params(script,config) {
     this.config = config
     this.script = script
-    this.params = input ( id: 'params', parameters: [
-      [$class: 'ChoiceParameterDefinition',name: 'CLUSTER_APP_DOMAIN',choices: 'https://master.marthanda.in',description: ''],
-      [$class: 'CredentialsParameterDefinition', credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl', defaultValue: '', description: '',name: 'openShiftToken',required: true]
-    ])
   }
 
   void getBuild() {
-      return this.params
+    this.script.input ( id: 'params', parameters: [
+      [$class: 'ChoiceParameterDefinition',name: 'CLUSTER_APP_DOMAIN',choices: 'https://master.marthanda.in',description: ''],
+      [$class: 'CredentialsParameterDefinition', credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl', defaultValue: '', description: '',name: 'openShiftToken',required: true]
+    ])
   }
 }
