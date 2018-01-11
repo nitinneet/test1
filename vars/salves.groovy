eae361640c84e4d6a87b73f8f70a3810135b30e5
root <root@templateservice.localdomain>
2018-01-11 17:24:28 +0530

    test: param from sharedlibrary

--- vars/salves.groovy
+++ vars/salves.groovy
@@ -1,19 +1,12 @@
 def call (slave){
 
-podTemplate(
-    cloud: 'mcluster',
-    label: 'maven-node',
-    serviceAccount: 'jenkins',
-    containers: [
-        containerTemplate(
-            name: 'maven', 
-            image: 'maven:3.3.9-jdk-8-alpine', 
-            ttyEnabled: true, 
-            command: 'cat'
-        )
-    ]
-)
-node('maven-node') {
+podTemplate(cloud: 'mcluster',label: 'anotherpod',  containers: [
+    containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-7-alpine')
+  ]) {
+}
+
+
+node('maven') {
 openshift.withCluster( 'mcluster' ) {
     openshift.withProject( 'anand' ) {
         echo "Hello from project: ${openshift.project()}"
