40ff4e27420e5f6933b0f4e8cff64d18870367c9
root <root@templateservice.localdomain>
2018-01-11 17:51:12 +0530

    test: param from sharedlibrary

--- vars/slaves.groovy
+++ vars/slaves.groovy
@@ -1,21 +1,25 @@
 #!/usr/bin/groovy
 def call() {
+
+openshift.withCluster( 'mcluster' ) {
+
+
   podTemplate(label: anand,
         containers: [
-        	containerTemplate(
-        		name: 'maven', 
-        		image: 'maven:3.3.9-jdk-8-alpine'
-        	)
+                containerTemplate(
+                        name: 'maven',
+                        image: 'maven:3.3.9-jdk-8-alpine'
+                )
         ]
   ) {
 
 node('anand') {
-openshift.withCluster( 'mcluster' ) {
+
     openshift.withProject( 'anand' ) {
         echo "Hello from project: ${openshift.project()}"
     }
-}
+
 }
 
-    }
+    } }
 }
