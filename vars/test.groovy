3227d09e6fbec0e154906d213b2973c27dd70c73
nitinneet <26272195+nitinneet@users.noreply.github.com>
2018-01-31 16:30:15 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,23 +1,18 @@
-import static org.k9.scm.Git.*
+import org.k9.scm.Git
 
-def call(body) 
-{
+def call(body) {
 
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
-    node {
-        // Clean workspace before doing anything
-        deleteDir()
 
-        try {
-            stage ('test') {
-		checkout()
-            }
-            stage ('Setup') {
-                git(url: "${config.giturl}", branch: "${config.branch}", credentialsId: "${config.credentialsId}")
-            }
+node {
+  adm = new Git(this,config)
+  adm.checkout()
 }
-   
+
+
+//compileDirectory("${config.script}", "${config.directory}")
+}
